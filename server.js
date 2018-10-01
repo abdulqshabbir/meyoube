782946c28f7bd13eaba6ead85b1000a4ac96b920
Abdul Shabbir <abdulshabbir@Abduls-MacBook-Air-2.local>
2018-10-01 21:28:18 +0800

    user preferences page created

--- server.js
+++ server.js
@@ -1,58 +1,8 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const dbUrl = 'mongodb://test:test123@ds259351.mlab.com:59351/free-code-camp-mongodb'
-const methodOverride = require('method-override')
-const connect        = require('connect')
-const ejs = require('ejs')
 
-//...Application Files
-const User = require('./models/User')
+app.use(express.static('public'));
 
-//================APP PREFERENCES ========================//
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-
-//================DATABASE CONNECTION ====================//
-
-mongoose.connect(dbUrl,{useNewUrlParser: true},(err, db) => {
-    if(err) {
-        console.log(err)
-        return
-    }
-    console.log('succesfully connected to database')
-
-
-    //================ INDEX ROUTE ========================//
-    app.get('/', (req, res) => {
-        res.render('home')
-    })
-
-    //================NEW USER ROUTE =====================//
-    app.get('/user', (req, res) => {
-        res.render('user')
-    })
-
-    app.get('/user/new', (req, res) => {
-        res.render('signup')
-    })
-
-    app.post('/user/new', (req, res) => {
-        User.create(req.body.user, (err, newUser) =>{
-            if(err) {
-                console.log(err)
-            }
-            else {
-                res.render('user', {user: req.body.user})
-            }
-        })
-        console.log("you're posting!")
-    })
-
-})
 app.listen(3000,function(){
     console.log("you are connected to port 3000");
     //test
